f1af10039a951bd8979be2466665e265f812d101
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-15 22:59:58 +0100

    Create ball prefabs & ball spawner

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject[] balls;
+
+	private void Start () {
+		InvokeRepeating ("SpawnBalls", 2f, 1.5f);
+	}
+
+	private void SpawnBalls () {
+		Instantiate(balls[Random.Range(0, balls.Length)]);
+	}
+}
