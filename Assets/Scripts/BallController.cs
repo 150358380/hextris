0814d607ae9ee861283eeaf92acdaf81a6592ebe
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-15 22:53:47 +0100

    Create ball objects & randomise spawn points

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float moveSpeed;
+
+
+	private Vector2 targetPos = new Vector2(0f, 0f);
+	private Vector2 startPos;
+	private float angleDelta = 60.0f;
+	private float starDis = 7.0f;
+
+	private void Start () {
+		float dir = (angleDelta * Random.Range (0, 6) + 30.0f) * Mathf.Deg2Rad;
+		startPos = new Vector2 (Mathf.Cos (dir), Mathf.Sin (dir)).normalized * starDis;
+		transform.position = startPos;
+	}
+
+	private void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
