b99734f285c356c94df081672dd7c44bceebe47e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-15 21:37:13 +0100

    Add player controller & write rotation logic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float smooth = 1f;
+	private float degree;
+	private float singleRotation = 60f;
+
+	private void Start () {
+
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			degree += singleRotation;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			degree -= singleRotation;
+		}
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler(0, 0, degree), 10 * smooth * Time.deltaTime);
+	}
+}
